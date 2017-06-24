e461f14cf4d5a31e7aa8a118b04dd5ac0ca0f92c
shungo11260 <shungotaniguchi1120@gmail.com>
2017-06-24 13:15:35 +0900

    inv_pend_testにおける倒立振子のPD制御

--- inv_pend_test.py
+++ inv_pend_test.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 # coding: utf-8
 
 import inverted_pendulum as ivtpnd
@@ -6,23 +6,28 @@ import inverted_pendulum as ivtpnd
 import threading
 import time
 
+import math
+
 if __name__ == "__main__":
-    
+
     pend = ivtpnd.InvertedPendulum(mass = 1.0,
                                    length = 1.0,
-                                   theta_0 = 2.0)
+                                   theta_0 = 1.0)
 
-    
+
     # pend.simulate(torque = 0.0,
     #               timestep = 0.001,
     #               num_steps = 5000)
 
+    theta_goal = math.asin(1)
+    theta_dot_goal = 0.0
+    Kp = 75.0
+    Kd = 3.0
+
     for time in range(5000):
         theta = pend.theta
         theta_dot = pend.theta_dot
-        pend.simulate_one_step(-40*theta-9*theta_dot , 0.001)
+        pend.simulate_one_step(Kp * (theta_goal - theta) + Kd * (theta_dot_goal - theta_dot) , 0.001)
 
 
     pend.plot()
-
-
