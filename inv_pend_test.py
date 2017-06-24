35c978c4e2ca054df281fd702f0e7e5f04487b61
shungo11260 <shungotaniguchi1120@gmail.com>
2017-06-25 02:45:45 +0900

    倒立振子の近似モデルの実装とそのテスト

--- inv_pend_test.py
+++ inv_pend_test.py
@@ -12,22 +12,26 @@ if __name__ == "__main__":
 
     pend = ivtpnd.InvertedPendulum(mass = 1.0,
                                    length = 1.0,
-                                   theta_0 = 1.0)
+                                   phi_0 = 0.2)
 
 
     # pend.simulate(torque = 0.0,
     #               timestep = 0.001,
     #               num_steps = 5000)
 
-    theta_goal = math.asin(1)
-    theta_dot_goal = 0.0
-    Kp = 75.0
+    phi_goal = 0.0
+    phi_dot_goal = 0.0
+    phi_integral = 0.0
+    Kp = 58.8  # Kp - mgl >= 0 より Kp >= 9.8
     Kd = 3.0
+    Ki = 0.0
+    timestep = 0.001
 
     for time in range(5000):
-        theta = pend.theta
-        theta_dot = pend.theta_dot
-        pend.simulate_one_step(Kp * (theta_goal - theta) + Kd * (theta_dot_goal - theta_dot) , 0.001)
-
+        phi = pend.phi
+        phi_dot = pend.phi_dot
+        phi_integral += phi*0.001
+        #pend.simulate_one_step(Kp * (phi_goal - phi) + Kd * (phi_dot_goal - phi_dot + Ki * (phi_goal*time*0.001 - phi_integral)) , 0.001)
+        pend.simulate_one_step(Kp, Kd, time, timestep)
 
     pend.plot()
--- inverted_pendulum.py
+++ inverted_pendulum.py
@@ -6,83 +6,123 @@ import matplotlib.pyplot as plt
 import math
 
 
-# theta_0[rad] equals zero when the pendulum is standing vertically. CW positive
+# phi_0[rad] equals zero when the pendulum is standing vertically. CW positive
 class InvertedPendulum:
 
     def __init__(self,
                  mass,
                  length,
-                 theta_0,
-                 theta_dot_0 = 0.0,
+                 phi_0,
+                 phi_dot_0 = 0.0,
                  gravitational_acc = 9.8,
                  store_old_data = True):
 
         self.m = mass
         self.l = length
-        self.theta = theta_0
-        self.theta_dot = theta_dot_0
+        self.phi = phi_0
+        self.phi_dot = phi_dot_0
         self.g = gravitational_acc
+        self.J = mass * length * length / 3.0
 
         self.store_old_data = store_old_data
-        self.theta_data = [self.theta] if store_old_data else []
-        self.theta_dot_data = [self.theta_dot] if store_old_data else []
+        self.phi_data = [self.phi] if store_old_data else []
+        self.phi_dot_data = [self.phi_dot] if store_old_data else []
         self.torque_data = []
         self.time_data = [0.0]
-        
+
     def simulate(self, torque, timestep, num_steps):
 
         for i in range(num_steps):
             self.simulate_one_step(torque, timestep)
 
-            
+
     # Simulate using RK4 (Runge-Kutta).
     # torque[Nm]
     # timestep[s]
-    def simulate_one_step(self, torque, timestep): 
+    def simulate_one_step(self, Kp, Kd, num, timestep):
 
-        tmp1 = torque * 3.0 / (self.m * self.l * self.l)
-        tmp2 = 3.0 * self.g / (2.0 * self.l)
+        time = timestep*num
+        torque  = Kp * self.phi + Kd * self.phi_dot
+        ganma = Kd / (self.J * 2)
+        omega = math.sqrt((Kp - self.m * self.g * self.l) / self.J)
+        if ganma > omega :
+            ganma2omega2 = math.sqrt(ganma*ganma - omega*omega)
+            if time == 0.0:
+                A = (self.phi + self.phi_dot * (ganma2omega2 + ganma)) / (2 * ganma2omega2)
+                B = self.phi - A
+            else :
+                A = (self.phi_data[0] + self.phi_dot_data[0] * (ganma2omega2 + ganma)) / (2 * ganma2omega2)
+                B = self.phi_data[0] - A
 
-        # k*: approximation of theta_dot
-        # l*: approximation of theta_ddot
-        k1 = self.theta_dot
-        l1 = tmp1 + tmp2 * math.sin(self.theta)
+            self.phi = math.exp(-ganma * time) * (A * math.exp(ganma2omega2 * time) + B * (math.exp(-ganma2omega2 * time)))
+            self.phi_dot = math.exp(-ganma * time) * (A * (ganma2omega2 - ganma) * math.exp(ganma2omega2 * time) - B * (ganma2omega2 + ganma) * math.exp(-ganma2omega2 * time))
 
-        k2 = self.theta_dot + timestep * l1 / 2.0
-        l2 = tmp1 + tmp2 * math.sin(self.theta + timestep * k1 / 2.0)
+        elif ganma < omega :
+            ganma2omega2 = math.sqrt(omega*omega - ganma*ganma)
+            if time == 0.0:
+                A = self.phi
+                B = (self.phi_dot + ganma*self.phi) / ganma2omega2
+            else :
+                A = self.phi_data[0]
+                B = (self.phi_dot_data[0] + ganma*self.phi_data[0]) / ganma2omega2
 
-        k3 = self.theta_dot + timestep * l2 / 2.0
-        l3 = tmp1 + tmp2 * math.sin(self.theta + timestep * k2 / 2.0)
+            self.phi = math.exp(-ganma * time) * (A*math.cos(ganma2omega2*time) + B*math.sin(ganma2omega2*time))
+            self.phi_dot = math.exp(-ganma * time) * ((-ganma*A + B*ganma2omega2)*math.cos(ganma2omega2*time) + (-ganma*B - A*ganma2omega2)*math.sin(ganma2omega2*time))
 
-        k4 = self.theta_dot + timestep * l3
-        l4 = tmp1 + tmp2 * math.sin(self.theta + timestep * k3)
+        else :
+            ganma2omega2 = 0.0
+            if time == 0.0:
+                A = self.phi
+                B = self.phi_dot + ganma*self.phi
+            else :
+                A = self.phi_data[0]
+                B = self.phi_dot_data[0] + ganma*self.phi_data[0]
+
+            self.phi = math.exp(-ganma * time) * (A + B*time)
+            self.phi_dot = math.exp(-ganma * time) *(B - ganma*A - ganma*B*time)
+
+        # k*: approximation of phi_dot
+        # l*: approximation of phi_ddot
+        """
+        k1 = self.phi_dot
+        l1 = tmp1 + tmp2 * math.sin(self.phi)
+
+        k2 = self.phi_dot + timestep * l1 / 2.0
+        l2 = tmp1 + tmp2 * math.sin(self.phi + timestep * k1 / 2.0)
+
+        k3 = self.phi_dot + timestep * l2 / 2.0
+        l3 = tmp1 + tmp2 * math.sin(self.phi + timestep * k2 / 2.0)
+
+        k4 = self.phi_dot + timestep * l3
+        l4 = tmp1 + tmp2 * math.sin(self.phi + timestep * k3)
 
         # update
-        self.theta = self.theta + timestep * (k1 + 2*k2 + 2*k3 + k4) / 6.0
-        self.theta_dot = self.theta_dot + timestep * (l1 + 2*l2 + 2*l3 + l4) / 6.0
+        self.phi = self.phi + timestep * (k1 + 2*k2 + 2*k3 + k4) / 6.0
+        self.phi_dot = self.phi_dot + timestep * (l1 + 2*l2 + 2*l3 + l4) / 6.0
+        """
 
         if self.store_old_data:
-            self.theta_data.append(self.theta)
-            self.theta_dot_data.append(self.theta_dot)
+            self.phi_data.append(self.phi)
+            self.phi_dot_data.append(self.phi_dot)
             self.torque_data.append(torque)
             self.time_data.append(self.time_data[-1] + timestep)
 
-            
+
     def plot(self):
 
         if not self.store_old_data:
             return
 
         plt.figure()
-        plt.plot(self.time_data, self.theta_data,
+        plt.plot(self.time_data, self.phi_data,
                  '.', markersize = 3)
         plt.xlabel("time [s]")
-        plt.ylabel("theta [rad]")
+        plt.ylabel("phi [rad]")
         plt.figure()
-        plt.plot(self.time_data, self.theta_dot_data,
+        plt.plot(self.time_data, self.phi_dot_data,
                  '.', markersize = 3)
         plt.xlabel("time [s]")
-        plt.ylabel("theta dot [rad/s]")
+        plt.ylabel("phi dot [rad/s]")
         plt.figure()
         plt.plot(self.time_data, self.torque_data + [self.torque_data[-1]],
                  '.', markersize = 3)
@@ -90,8 +130,3 @@ class InvertedPendulum:
         plt.ylabel("torque [Nm]")
 
         plt.show()
-        
-
-
-
-        
