e1267ecc98c0c5cfda293238aaf97bc0e520965d
shungo11260 <shungotaniguchi1120@gmail.com>
2017-06-26 19:45:31 +0900

    PD制御の可視化

--- inv_pend_test.py
+++ inv_pend_test.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 # coding: utf-8
 
 import inverted_pendulum as ivtpnd
@@ -7,22 +7,26 @@ import threading
 import time
 
 if __name__ == "__main__":
-    
+
     pend = ivtpnd.InvertedPendulum(mass = 1.0,
                                    length = 1.0,
-                                   theta_0 = 2.0)
+                                   theta_0 = 0.5)
 
-    
+
     # pend.simulate(torque = 0.0,
     #               timestep = 0.001,
     #               num_steps = 5000)
 
+    Kp = 40.0
+    Kd = 9.0
+    theta_goal = 0.0
+    theta_dot_goal = 0.0
+
     for time in range(5000):
         theta = pend.theta
         theta_dot = pend.theta_dot
-        pend.simulate_one_step(-40*theta-9*theta_dot , 0.001)
+        torque = Kp*(theta_goal-theta)+Kd*(theta_dot_goal-theta_dot)
+        pend.simulate_one_step(torque , 0.001)
 
 
     pend.plot()
-
-
